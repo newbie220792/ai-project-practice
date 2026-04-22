a01545ee34149a4dc755dd97bbd815083f23c963
Anthony.Doan <anthony.doan@code88.asia>
2026-04-22 17:51:52 +0700

    capture img

--- imou_main.py
+++ imou_main.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+import time
+
+from flask import Flask, jsonify, request, Response
 import psycopg2
-# import mysql.connector
 import os
 from dotenv import load_dotenv
 from psycopg2.extras import Json
+import requests
+import cv2
+import base64
 
 app = Flask(__name__)
 
@@ -14,7 +18,6 @@ BLACKLIST_CAMERAS = set(['Nhà 2', 'Nhà 1'])  # Danh sách đen để lưu tr�
 @app.route('/callback', methods=['POST','GET', 'PUT', 'DELETE'])
 def callback():
     data = request.json  # nhận JSON từ server gửi tới
-    # print("Received body:", data)  # in ra phần body của JSON
 
     return  post_data(data)
     # xử lý logic ở đây
@@ -32,26 +35,31 @@ def post_data(data):
             password=os.getenv("DB_PASSWORD")
         )
         cur = conn.cursor()
-        # raw_data  = Json(data);
+
         alarmId= data.get("alarmId");
         dname= data.get("dname");
         msgType = data.get("msgType");
         thumbUrl = data.get("thumbUrl");
         time = data.get("time");
+        did = data.get("did");
 
         if dname in BLACKLIST_CAMERAS:
             print(f"Camera {dname} is blacklisted. Skipping save.")
             return jsonify({"status": f"Camera '{dname}' is blacklisted. Skipping save."}), 200
         
+        capture_image_from_camera(did)
         cur.execute(
-            "INSERT INTO imou_camera_log (alarm_id, dname, msg_type, thumb_url, data, created_at) VALUES (%s, %s, %s, %s, %s, %s)",
+            "INSERT INTO imou_camera_log (alarm_id, dname, msg_type, thumb_url, data, created_at, device_id, img) " \
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
             (
                 alarmId,
                 dname,
                 msgType,
                 thumbUrl,
                 Json(data),
-                time
+                time,
+                did,
+                "http://example.com/default_image.jpg"
             )
         )
         
@@ -62,6 +70,64 @@ def post_data(data):
     except Exception as e:
         print(str(e));
         return jsonify({"error": str(e)}), 500
+
+def capture_image_from_camera(camera_id):
+    # Define IP mapping for different cameras
+    camera_ip = {
+        "A4562BCPSFDFF1A": "192.168.1.225", #cổng
+        "06F2EBDPSF0A55F": "192.168.1.221", #phòng khách
+        "C9804BJPSF67B3E": "192.168.1.102", #phòng ngủ
+        "C9804BJPSF52581": "192.168.1.117", #phòng bếp
+        "C9804BJPSF07E00": "192.168.1.143", #tầng 2
+    }
+
+    ip = camera_ip.get(camera_id)  # nếu không có thì dùng luôn IP
+    if not ip :
+        raise ValueError(f"Camera ID '{camera_id}' is not recognized or does not have an associated IP address");
+    
+    username = os.getenv("CAMERA_USERNAME", "admin")
+    password = os.getenv("CAMERA_PASSWORD", "password")
+
+    if not username or not password:
+        raise ValueError("Camera credentials are not set in environment variables")
+    
+    url = f"rtsp://{username}:{password}@{ip}:554/cam/realmonitor?channel=1&subtype=0"
+    print(f"Connecting to camera at {url}")
+    
+    cap = cv2.VideoCapture(url)
+    ret, frame = cap.read()
+
+    timestamp = int(time.time())
+
+    OUTPUT_FOLDER = "/home/rasp/Desktop/imou/capture_imou"
+    filepath = os.path.join(OUTPUT_FOLDER, f"{camera_id}_{timestamp}.jpg")
+
+    if ret:
+        os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+        cv2.imwrite(filepath, frame)
+    else:
+       raise ValueError("Failed to capture image from camera")
+
+    cap.release()
+
+def decrypt(encryptedValue):
+    import base64
+    from cryptography.fernet import Fernet
+
+    salt = os.getenv("DECRYPT_SALT", "default_salt")
+    decrypted_bytes = base64.b64decode(encryptedValue.encode())
+    cipher = Fernet(base64.urlsafe_b64encode(salt.encode().ljust(32)[:32]))
+    return cipher.decrypt(decrypted_bytes).decode()
+
+
+def encrypt(plainValue):
+    import base64
+    from cryptography.fernet import Fernet
+
+    salt = os.getenv("DECRYPT_SALT", "default_salt")
+    cipher = Fernet(base64.urlsafe_b64encode(salt.encode().ljust(32)[:32]))
+    encrypted_bytes = cipher.encrypt(plainValue.encode())
+    return base64.b64encode(encrypted_bytes).decode()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9090)
